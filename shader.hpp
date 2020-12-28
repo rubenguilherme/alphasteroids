5017d00ed8460c41e2c328b1bc9f6d69f05baf2d
pduarte21 <p.rbduarte21@gmail.com>
2020-12-28 16:50:21 +0000

    Added shaders

---
+++ shader.hpp
@@ -0,0 +1,15 @@
+//
+//  shader.hpp
+//  Ex1
+//
+//  Created by Paulo Duarte on 28/09/2020.
+//
+
+#include <GL/glew.h>
+#include <OpenGL/gl.h>
+#ifndef SHADER_HPP
+#define SHADER_HPP
+
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
+
+#endif
